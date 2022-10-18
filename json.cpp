d49e4a21dbf2155aa2d19073a1e00914cc28e38e
Jonathan Karlsson <er.jonathan.karlsson@hotmail.com>
2022-10-19 00:06:23 +0200

    Finding a design.

--- json.cpp
+++ json.cpp
@@ -1,12 +1,12 @@
 #include "json.h"
 
-char json::data_type::ch(uint64_t x)
+char json::parse::to_ch(uint64_t x)
 {
 	static const char DIGITS[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 	return DIGITS[x];
 }
 
-uint64_t json::data_type::num(char ch)
+uint64_t json::parse::to_num(char ch)
 {
 	switch (ch) {
 	case '0': return 0;
@@ -23,12 +23,12 @@ uint64_t json::data_type::num(char ch)
 	return -1;
 }
 
-bool json::data_type::is_num(char ch)
+bool json::parse::is_num(char ch)
 {
-	return num(ch) >= 0;
+	return to_num(ch) >= 0;
 }
 
-bool json::data_type::is_white(char ch)
+bool json::parse::is_white(char ch)
 {
 	switch (ch) {
 	case ' ':
@@ -40,7 +40,7 @@ bool json::data_type::is_white(char ch)
 	return false;
 }
 
-uint64_t json::data_type::skip_white(const char *s)
+uint64_t json::parse::skip_white(const char *s)
 {
 	uint64_t caret = 0;
 	while (s[caret] != 0 && is_white(s[caret])) {
@@ -49,7 +49,7 @@ uint64_t json::data_type::skip_white(const char *s)
 	return caret;
 }
 
-bool json::data_type::is_match(const char *s1, const char *s2)
+bool json::parse::is_match(const char *s1, const char *s2)
 {
 	while (s1 != 0 && s2 != 0) {
 		if (*(s1++) != *(s2++)) {
@@ -59,13 +59,14 @@ bool json::data_type::is_match(const char *s1, const char *s2)
 	return true;
 }
 
-char *json::data_type::print(char *dst, const char *src)
+char *json::parse::print(char *dst, const char *src)
 {
 	while (*src != 0) {
 		*(dst++) = *(src++);
 	}
 	return dst;
 }
+
 
 json::int_type::int_type( void ) : value(0) {}
 
@@ -76,11 +77,11 @@ json::type_id json::int_type::json_type( void ) const
 	return json::TYPEID_INT;
 }
 
-json::result<const char*> json::int_type::json_read(const char *json)
+json::result<const char*> json::int_type::read_json(const char *json)
 {
-	uint64_t caret = skip_white(json);
+	uint64_t caret = parse::skip_white(json);
 	const uint64_t c = caret;
-	while (is_num(*json)) {
+	while (parse::is_num(*json)) {
 		++json;
 	}
 	if (c == caret) {
@@ -91,7 +92,7 @@ json::result<const char*> json::int_type::json_read(const char *json)
 	}
 	value = 0;
 	for (int64_t i = 0; i < caret; ++i) {
-		value = value * 10 + num(json[i]);
+		value = value * 10 + parse::to_num(json[i]);
 	}
 	return result<const char*>{
 		json + caret,
@@ -99,7 +100,7 @@ json::result<const char*> json::int_type::json_read(const char *json)
 	};
 }
 
-char *json::int_type::json_write(char *json_out) const
+char *json::int_type::print_json(char *json_out) const
 {
 	uint64_t caret = 0;
 	if (value < 0) {
@@ -107,7 +108,7 @@ char *json::int_type::json_write(char *json_out) const
 	}
 	uint64_t x = abs(value);
 	while (x >= 0) {
-		json_out[caret++] = ch(x % 10);
+		json_out[caret++] = parse::to_ch(x % 10);
 		x /= 10;
 	}
 	return json_out + caret;
@@ -124,23 +125,98 @@ uint64_t json::int_type::json_len( void ) const
 	return digits;
 }
 
+
+json::float_type::float_type( void ) : value(0.0)
+{}
+
+json::float_type::float_type(double val) : value(val)
+{}
+
 json::type_id json::float_type::json_type( void ) const
 {
 	return json::TYPEID_FLOAT;
 }
+
+json::result<const char*> json::float_type::read_json(const char *json_in)
+{
+	return json::result<const char*>{
+		json_in,
+		false
+	};
+}
+
+char *json::float_type::print_json(char *json_out) const
+{
+	// TODO: number can start with + or -
+	// TODO: E/e can start with + or -
+	uint64_t caret = json::parse::skip_white(json_out);
+
+	int64_t i = 0;
+	while (caret < len && json::parse::is_num(json_out[caret])) {
+		i = i * 10 + json::parse::to_num(json_out[caret]);
+		++caret;
+	}
+
+	double f = double(i);
+	if (caret < len && json_out[caret] == '.') {
+		++caret;
+		double r = 0;
+		uint64_t d = 0;
+		while (caret < len && json::parse::is_num(json_out[caret])) {
+			r = r * 10 + json::parse::to_num(json_out[caret]);
+			++caret;
+			++d;
+		}
+		for (uint64_t j = 0; j < d; ++j) {
+			f *= 0.1;
+		}
+		f += r;
+	}
+	if (caret < len && json_out[caret] == 'e' || json_out[caret] == 'E') {
+		++caret;
+		int64_t e = 0;
+		while (caret < len && json::parse::is_num(json_out[caret])) {
+			e = e * 10 + json::parse::to_num(json_out[caret]);
+			++caret;
+		}
+		if (e >= 0) {
+			for (int64_t j = 0; j < e; ++j) {
+				f = f * 10.0;
+			}
+		} else {
+			for (int64_t j = 0; j > e; --j) {
+				f = f * 0.1;
+			}
+		}
+	}
+
+	return json_out + caret;
+}
+
+uint64_t json::float_type::json_len( void ) const
+{
+	return 0;
+}
+
+
+json::bool_type::bool_type( void ) : value(false)
+{}
+
+json::bool_type::bool_type(bool val) : value(val)
+{}
 
 json::type_id json::bool_type::json_type( void ) const
 {
 	return json::TYPEID_BOOL;
 }
 
-json::result<const char*> json::bool_type::json_read(const char *json)
+json::result<const char*> json::bool_type::read_json(const char *json)
 {
-	uint64_t caret = skip_white(json);
-	if (is_match(json + caret, "true")) {
+	uint64_t caret = parse::skip_white(json);
+	if (parse::is_match(json + caret, "true")) {
 		value = true;
 		caret += 4;
-	} else if (is_match(json + caret, "false")) {
+	} else if (parse::is_match(json + caret, "false")) {
 		value = false;
 		caret += 5;
 	} else {
@@ -155,9 +231,9 @@ json::result<const char*> json::bool_type::json_read(const char *json)
 	};
 }
 
-char *json::bool_type::json_write(char *json_out) const
+char *json::bool_type::print_json(char *json_out) const
 {
-	return value ? print(json_out, "true") : print(json_out, "false");
+	return value ? parse::print(json_out, "true") : parse::print(json_out, "false");
 }
 
 uint64_t json::bool_type::json_len( void ) const
@@ -165,30 +241,48 @@ uint64_t json::bool_type::json_len( void ) const
 	return value ? 4 : 5;
 }
 
+
+json::string_type::string_type( void ) : value(nullptr), length(0)
+{}
+
+json::string_type::string_type(const char *val, uint64_t len) : value(val), length(len)
+{}
+
 json::type_id json::string_type::json_type( void ) const
 {
 	return json::TYPEID_STR;
 }
+
+json::result<const char*> json::string_type::read_json(const char *json_in)
+{
+	return json::result<const char*>{
+		json_in,
+		false
+	};
+}
+
 
 json::type_id json::array_type::json_type( void ) const
 {
 	return json::TYPEID_ARR;
 }
 
+
 json::type_id json::object_type::json_type( void ) const
 {
 	return json::TYPEID_OBJ;
 }
 
-json::type_id json::nil_type::json_type( void ) const
+
+json::type_id json::null_type::json_type( void ) const
 {
-	return json::TYPEID_NIL;
+	return json::TYPEID_NULL;
 }
 
-json::result<const char*> json::nil_type::json_read(const char *json)
+json::result<const char*> json::null_type::read_json(const char *json)
 {
-	uint64_t caret = skip_white(json);
-	if (!is_match(json, "null")) {
+	uint64_t caret = parse::skip_white(json);
+	if (!parse::is_match(json, "null")) {
 		return result<const char*>{
 			json + caret,
 			false
@@ -200,12 +294,91 @@ json::result<const char*> json::nil_type::json_read(const char *json)
 	};
 }
 
-char *json::nil_type::json_write(char *json_out) const
+char *json::null_type::print_json(char *json_out) const
 {
-	return print(json_out, "null");
+	return parse::print(json_out, "null");
 }
 
-uint64_t json::nil_type::json_len( void ) const
+uint64_t json::null_type::json_len( void ) const
 {
 	return 4;
 }
+
+
+char *json::print(char *json_out, int64_t value)
+{
+	return json::int_type(value).print_json(json_out);
+}
+
+char *json::print(char *json_out, double value)
+{
+	return json::float_type(value).print_json(json_out);
+}
+
+char *json::print(char *json_out, bool value)
+{
+	return json::bool_type(value).print_json(json_out);
+}
+
+char *json::print(char *json_out, const char *value, uint64_t len)
+{
+	return json::string_type(value, len).print_json(json_out);
+}
+
+char *json::print(char *json_out, const data_type &type)
+{
+	return type.print_json(json_out);
+}
+
+/*json::data_type *tokenize_num(const char *json, uint64_t len)
+{
+	// TODO: number can start with + or -
+	// TODO: E/e can start with + or -
+	uint64_t caret = json::parse::skip_white(json);
+
+	int64_t i = 0;
+	while (caret < len && json::parse::is_num(json[caret])) {
+		i = i * 10 + json::parse::to_num(json[caret]);
+		++caret;
+	}
+	bool is_float = false;
+
+	double f = double(i);
+	if (caret < len && json[caret] == '.') {
+		++caret;
+		double r = 0;
+		uint64_t d = 0;
+		while (caret < len && json::parse::is_num(json[caret])) {
+			r = r * 10 + json::parse::to_num(json[caret]);
+			++caret;
+			++d;
+		}
+		for (uint64_t j = 0; j < d; ++j) {
+			f *= 0.1;
+		}
+		f += r;
+		is_float = true;
+	}
+	if (caret < len && json[caret] == 'e' || json[caret] == 'E') {
+		++caret;
+		int64_t e = 0;
+		while (caret < len && json::parse::is_num(json[caret])) {
+			e = e * 10 + json::parse::to_num(json[caret]);
+			++caret;
+		}
+		if (e >= 0) {
+			for (int64_t j = 0; j < e; ++j) {
+				f = f * 10.0;
+			}
+		} else {
+			for (int64_t j = 0; j > e; --j) {
+				f = f * 0.1;
+			}
+		}
+		is_float = true;
+	}
+
+	return !is_float ? json::int_type(i) : json::float_type(f);
+}*/
+
+
--- json.h
+++ json.h
@@ -10,13 +10,24 @@ namespace json
 	// Constants representing all JSON types.
 	enum type_id
 	{
-		TYPEID_NIL,
+		TYPEID_NULL,
 		TYPEID_BOOL,
 		TYPEID_INT,
 		TYPEID_FLOAT,
 		TYPEID_ARR,
 		TYPEID_STR,
 		TYPEID_OBJ
+	};
+
+	namespace parse
+	{
+		char      to_ch(uint64_t x);
+		uint64_t  to_num(char ch);
+		bool      is_num(char ch);
+		bool      is_white(char ch);
+		uint64_t  skip_white(const char *s);
+		bool      is_match(const char *s1, const char *s2);
+		char     *print(char *dst, const char *src);
 	};
 
 	// The result from a parsing operation.
@@ -30,24 +41,15 @@ namespace json
 	// The base class for all data types in JSON.
 	class data_type
 	{
-	protected:
-		static char ch(uint64_t x);
-		static uint64_t num(char ch);
-		static bool is_num(char ch);
-		static bool is_white(char ch);
-		static uint64_t skip_white(const char *s);
-		static bool is_match(const char *s1, const char *s2);
-		static char *print(char *dst, const char *src);
-
 	public:
 		// Returns the specific type of the data.
 		virtual type_id json_type( void ) const = 0;
 
 		// Reads a value from an input stream.
-		virtual result<const char*> json_read(const char *json) = 0;
+		virtual result<const char*> read_json(const char *json) = 0;
 
 		// Writes the value to an out stream.
-		virtual char* json_write(char *out_json) const = 0;
+		virtual char* print_json(char *out_json) const = 0;
 
 		// Computes the length of the JSON string.
 		virtual uint64_t json_len( void ) const = 0;
@@ -65,8 +67,8 @@ namespace json
 		int_type &operator=(const int_type&) = default;
 
 		type_id              json_type( void ) const;
-		result<const char*>  json_read(const char *json);
-		char                *json_write(char *json_out) const;
+		result<const char*>  read_json(const char *json);
+		char                *print_json(char *json_out) const;
 		uint64_t             json_len( void ) const;
 	};
 
@@ -81,7 +83,10 @@ namespace json
 		float_type(const float_type&) = default;
 		float_type &operator=(const float_type&) = default;
 
-		type_id json_type( void ) const;
+		type_id              json_type( void ) const;
+		result<const char*>  read_json(const char *json);
+		char                *print_json(char *json_out) const;
+		uint64_t             json_len( void ) const;
 	};
 
 	class bool_type : public data_type
@@ -90,21 +95,34 @@ namespace json
 		bool value;
 
 	public:
+		bool_type( void );
+		bool_type(bool val);
+		bool_type(const bool_type&) = default;
+		bool_type &operator=(const bool_type&) = default;
+
 		type_id              json_type( void ) const;
-		result<const char*>  json_read(const char *json);
-		char                *json_write(char *json_out) const;
+		result<const char*>  read_json(const char *json);
+		char                *print_json(char *json_out) const;
 		uint64_t             json_len( void ) const;
 	};
 
 	class string_type : public data_type
 	{
 	private:
-		char *value;
+		char     *value;
+		uint64_t  length;
 
 	public:
+		string_type( void );
+		string_type(const char *val, uint64_t len);
+		string_type(const string_type&) = default;
+		string_type &operator=(const string_type&) = default;
 		~string_type( void );
 
-		type_id json_type( void ) const;
+		type_id              json_type( void ) const;
+		result<const char*>  read_json(const char *json);
+		char                *print_json(char *json_out) const;
+		uint64_t             json_len( void ) const;
 	};
 
 	class array_type : public data_type
@@ -129,15 +147,20 @@ namespace json
 	};
 
 	// The data type for null values.
-	class nil_type : public data_type
+	class null_type : public data_type
 	{
 	public:
 		type_id              json_type( void ) const;
-		result<const char*>  json_read(const char *json);
-		char                *json_write(char *json_out) const;
+		result<const char*>  read_json(const char *json);
+		char                *print_json(char *json_out) const;
 		uint64_t             json_len( void ) const;
 	};
 
+	char *print(char *json_out, int64_t value);
+	char *print(char *json_out, double value);
+	char *print(char *json_out, bool value);
+	char *print(char *json_out, const char *value, uint64_t len);
+	char *print(char *json_out, const data_type &value);
 }
 
 #endif
